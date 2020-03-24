31c297fa3ed4f7dd9fea32b7d04457b527221b54
azinum <lukaska61@gmail.com>
2020-03-24 23:05:37 +0100

    Change bar and border color

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char selbgcolor[]      = "#005577";
-static const char selbordercolor[]  = "#00a5b7";
+static const char selbgcolor[]      = "#098A9E";
+static const char selbordercolor[]  = "#098A9E";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 
