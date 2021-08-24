a1dffccf4d620bc484befa909753168d81d9d5f9
azinum <lukaska61@gmail.com>
2021-08-24 21:10:34 +0200

    Update dwmblocks status bar config

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -7,16 +7,16 @@ static unsigned int delimLen = 5;
 
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/* Icon */	/* Command */		/* Update Interval */	/* Update Signal */
+  /* Icon */	/* Command */		/* Update Interval */	/* Update Signal */
   {"\n", "", 60, 0},
 
   {"", "keylayout", 60, 2},
 
-	{"mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  {"mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
   {"", "sed 's/down/🌎❌/;s/up/🌎/' /sys/class/net/e*/operstate 2>/dev/null", 20, 0},
 
   {"", "stat-volume", 30, 1},
 
-	{"", "date '+%a %d %b %Y %H:%M:%S'",					1,		0},
+  {"", "date '+%a %d %b %Y %H:%M:%S'",					1,		0},
 };
